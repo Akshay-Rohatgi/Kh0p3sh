717b6951a45c7194ad614621816a3116025bb091
Akshay-Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-07-03 21:50:05 -0700

    made it look cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import getpass
-getpass.getuser()
+user = getpass.getuser()
 
 
 print('''
@@ -16,23 +16,50 @@ print('''
 ==========================================================
 
     ''')
+print('Welcome to Kh0p3sh, a commandline tool built for penetration testers.')
+print('======================================================================')
 
 print('Kh0p3sh Console:')
 print('For list of commands type help')
 
 while True:
-    command = input('>')
+    command = input('\033[1;31;40m' + user + '\033[0m' + '\033[1;34;40m@Kh0p3sh-console\033[0m>')
     if str(command) == 'help':
-        print('Available Commands:')
         print('''
-        help - shows available commands and their uses
-        exit - will exit out of the Kh0p3sh Console
-        hax0r - will spam cool text to make it look like you are an epic hacker
+    
+    ==================
+    Available Commands
+    ==================
+
+    ==============================================
+    Neutral Commands
+    ----------------------------------------------
+    help - shows available commands and their uses
+    exit - will exit out of the Kh0p3sh Console
+    hax0r - give you encouraging thoughts!
+    ==============================================
+
+    ==============================================
+    Vulnerability Assessment Commands
+    ----------------------------------------------
+    firewall - will check if firewall is up
+    findSUID - will list all SUID files found
+    findGUID - will list all GUID files found
+    getRules - will list all iptables rules
+    get
+    ==============================================
+
         ''')
     elif str(command) == 'exit':
         print('Goodbye!')
         break
     elif str(command) == 'hax0r':
         print('you are a hax0r')
+    
+#################END OF NEUTRAL COMMANDS##################
+
+
+
+
     else:
         print('Command does not exist! try the "help" command')
